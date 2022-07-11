f6d8ae07d04704fff76e321fdea6f6f162c5c463
lixiyuan <20191112413@stu.sspu.edu.cn>
2022-07-11 12:46:58 +0800

    debug

--- util/request_handler/use.py
+++ util/request_handler/use.py
@@ -114,10 +114,10 @@ def compute(region: Optional[str], year_month: Optional[str]) -> str:
         achievement = predict_records_total_box / reality_records_total_box * 100
     if reality_records_total_bottle != 0:
         bottle_achievement = predict_records_total_bottle / reality_records_total_bottle * 100
-    return "蓝气罐预销|".center(15) + "实销|".center(15) + "差额|".center(15) + "达成率|\n".center(15) \
+    return "蓝气罐预销|".center(20) + "实销|".center(20) + "差额|".center(20) + "达成率|\n".center(20) \
            + (str(predict_records_total_box)+"箱").rjust(15) + \
            (str(predict_records_total_bottle)+"箱").rjust(15) \
            + (str(balance)+"箱").rjust(15) + (str(achievement)+"%\n").rjust(15) \
            + "轻饮酒预销 |".center(15) + "实销 |".center(15) + "差额 |".center(15) + "达成率 |\n".center(15) \
-           + str(predict_records_total_bottle).center(8)+"瓶  |  ".center(7) + str(reality_records_total_bottle).center(8)+"瓶  |  ".center(7) \
-           + str(bottle_balance).center(8) + "瓶  |  ".center(7) + str(bottle_achievement).center(8) + "%   | ".center(7)
+           + (str(predict_records_total_bottle)+"瓶").rjust(15) + (str(reality_records_total_bottle)+"瓶").rjust(15)+ \
+           (str(bottle_balance)+"瓶").rjust(15) + (str(bottle_achievement)+"%").center(15)
